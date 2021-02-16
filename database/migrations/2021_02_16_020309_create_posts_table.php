a865a3bbf4c0df04ce42e3b126791f0a11713db4
Andre Leroy-Beaulieu <andreleroy@Andres-MacBook-Pro.local>
2021-02-15 20:44:12 -0600

    Add error logging, create Image and Post models and migrations

---
+++ database/migrations/2021_02_16_020309_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('caption', 300);
+            $table->string('imageUrl', 2083);
+            $table->string('owner', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
